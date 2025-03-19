f820429e8f0f83e367515b613dc9f03453b37c9a
erfan mahdavi <s.m.e.m1383@gmail.com>
2025-03-19 07:47:50 +0330

    added comment for bfs solver

--- uninformed_search.py
+++ uninformed_search.py
@@ -2,8 +2,8 @@ from collections import deque
 
 class BFSSolver:
     """
-    we use breadth-first search to find the shortest path from (0,0) to (n-1, n-1)
-     ignore all coin gains or thief encounters
+    we use breadth-first search to find the shortest path from (0,0) to (n-1, n-1) ignore all coin gains or thief encounters
+    we assume aryan has no knowledge about map being squared or exit point being at (n-1, n-1)
     """
     def __init__(self, grid_size):
         self.grid_size = grid_size
