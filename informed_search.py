d6c6c2fa882d10bc73d3a97d96bf4b92ce881d20
Taraneh <145665647+Taraneh-trk@users.noreply.github.com>
2025-03-24 11:59:12 +0330

    add main structure of informed searches in part 2 and part 3

---
+++ informed_search.py
@@ -0,0 +1,22 @@
+from queue import PriorityQueue
+
+
+class Astar:
+    def __init__(self,n,grid):
+        self.n = n
+        self.grid = grid
+
+    def heuristic_1(self):
+        ...
+
+    def cost_1(self):
+        ...
+
+    def heuristic_2(self):
+        ...
+
+    def cost_2(self):
+        ...
+
+    def A_star(self):
+        ...
