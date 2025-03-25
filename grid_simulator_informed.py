62a60561211d75ed8c431a5c42927ce0a3b869a1
erfan mahdavi <s.m.e.m1383@gmail.com>
2025-03-25 08:52:04 +0330

    delete grid_simulator and add informedSimulator body

--- grid_simulator_informed.py
+++ grid_simulator_informed.py
@@ -1,5 +1,13 @@
 '''
-class Gridsimulator_uninformed :
-    def __init__(self,grid,step,)
+class simulatorState :
+    # create an object of this class for each state
+    def __init__(self, pos, coins_collected, coins_stolen, grid, has_thief)
     
+    
+    
+def initial_state(grid)
+    return simulaatorState_object
+    
+def initial_state(grid, direction, n)
+    return simulaatorState_object
 '''
